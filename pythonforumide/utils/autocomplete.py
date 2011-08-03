2b3c6b7b81e7a1f4e08afd81da816a265249edf5
bunburya <bunburya@tcd.ie>
2011-08-03 23:44:27 +0100

    half-way through implementing some changes DO NOT MERGE OR PUSH

--- pythonforumide/utils/autocomplete.py
+++ pythonforumide/utils/autocomplete.py
@@ -2,6 +2,8 @@
 @author: bunburya
 """
 
+class NoSuchNamespaceError(Exception): pass
+
 # maybe move these to a separate file eventually
 keywords = set(['and ', 'elif ', 'is ', 'global ', 'pass', 'if ',
     'from ', 'raise ', 'for ', 'except ', 'finally', 'print ',
@@ -18,28 +20,41 @@ class CodeCompletion(object):
     
     def __init__(self, *modules):
         self._suggestions = set()
+        self._namespaces = {'': set()} # empty string represents global namespace
+        self._current_ns = ''
         self._cache = set()
         self._key = []
+        self._context_ns = []
         for mod in modules:
             self.add_module(mod)
     
     def add_builtins(self):
-        self._suggestions.update(builtins)
+        self._namespaces['__builtins__'] = builtins
+        self._namespaces[''].update(builtins)
     
     def add_keywords(self):
-        self._suggestions.update(keywords)
+        self._namespaces[''].update(keywords)
     
     def add_module(self, module):
         """Adds the variable and method names from a module to the pool
         of potential suggestions.
         """
-        self._suggestions.update(dir(module))
+        self._namespaces[module.__name__] = dir(module)
     
-    def add_suggestion(self, *suggest):
+    def add_suggestions(self, suggest, ns=''):
         """Takes at least one string as an argument; adds each argument
         to the pool of potential suggestions.
         """
-        self._suggestions.update(suggest)
+        if ns in self._namespaces:
+            self._namespaces[ns].update(suggest)
+        else:
+            self._namespaces[ns] = suggest
+    
+    def add_suggestion(self, suggest, ns=''):
+        if ns in self._namespaces:
+            self._namespaces[ns].add(suggest)
+        else:
+            self._namespaces[ns] = set((suggest,))
     
     def suggest(self, key=None):
         """Return a set of possible completions based on the keyword
@@ -50,22 +65,33 @@ class CodeCompletion(object):
             key = ''.join(self._key)
         if not key:
             return set()
-        pool = self._cache or self._suggestions
+        pool = self._cache or self._namespaces[self._current_ns]
         suggs = set(s for s in pool if s.startswith(key))
         self._cache = set(suggs)
         return suggs
         
     def update_key(self, char):
-        if not char in self.valid_ch:
+        if char == '.':
+            self.switch_ns(self.key)
+            self._context_ns.append(self.key)
+            print self._context_ns
+            self.clear_key()
+        elif not char in self.valid_ch:
             self.clear()
         else:
             self._key.append(char)
     
     def back(self):
         try:
-            self._key.pop()
+            char = self._key.pop()
         except IndexError:
-            pass
+            return
+        if char == '.':
+            try:
+                self.key = self._context_ns.pop()
+                print self.key
+            except IndexError:
+                pass
     
     def cache(self, c):
         self._cache = c
@@ -79,6 +105,13 @@ class CodeCompletion(object):
     def clear(self):
         self._cache = set()
         self._key = []
+    
+    def switch_ns(self, new):
+        if new in self._namespaces:
+            self._current_ns = new
+            self.clear_cache()
+        else:
+            raise NoSuchNamespaceError(new)
     
     @property
     def choices(self):
