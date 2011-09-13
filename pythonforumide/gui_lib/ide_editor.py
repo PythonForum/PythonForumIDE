63bb7665be181696e9d412bc9865d3e98c5528f3
David Gomes <davidrafagomes@gmail.com>
2011-09-13 13:55:32 +0100

    Removed testing code from master

--- pythonforumide/gui_lib/ide_editor.py
+++ pythonforumide/gui_lib/ide_editor.py
@@ -254,16 +254,3 @@ class Editor(stc.StyledTextCtrl):
         """Save the current file as a new filepath"""
         self.filepath= filepath
         return self.save_file()
-
-if __name__=='__main__':
-    """Adds the editor to the frame"""
-    import ide_test_app as wx_app
-    import ide_simple_frame
-    app = wx_app.Wx_App(False)
-    frame= ide_simple_frame.SimpleFrame(None, title= "Testing editor")
-    panel= ide_simple_frame.TestPanel(frame)
-    frame.sizer.Add(panel, 1, wx.EXPAND)
-    editor= Editor(panel)
-    panel.sizer.Add(editor, 1, wx.EXPAND)
-    frame.Layout()
-    app.MainLoop()
